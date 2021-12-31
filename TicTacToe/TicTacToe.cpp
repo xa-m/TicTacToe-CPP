9744018594d7e668b014c9ca1f7902be5b97190c
Muhammet Ali <muhammetwas@gmail.com>
2021-12-31 22:11:24 +0300

    changed extern position

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,11 +1,11 @@
 // TicTacToe.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <iostream>
-#include "Game.h"
 extern "C" {
 #include "Message.h"
 }
+
+#include <iostream>
+#include "Game.h"
 
 int main()
 {
